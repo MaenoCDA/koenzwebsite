cec94919e4576d590a2fb650808be9f8aefd3981
jeroen <jeroenweening04@gmail.com>
2025-10-16 13:35:39 +0200

    fix: add TLS certificate workaround to server.js

--- apps/cms/server.js
+++ apps/cms/server.js
@@ -1,3 +1,6 @@
+// Fix for self-signed certificate issues in production
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const strapi = require('@strapi/strapi');
 
 const app = strapi.createStrapi({ distDir: './dist' });
