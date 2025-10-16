6b00f8b04d9e2f872083c8e608a3989737ea2897
jeroen <jeroenweening04@gmail.com>
2025-10-16 13:47:46 +0200

    fix: comprehensive TLS certificate fixes for Strapi

--- apps/cms/server.js
+++ apps/cms/server.js
@@ -1,6 +1,9 @@
 // Fix for self-signed certificate issues in production
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+// Global HTTPS fix for all requests
+require('https').globalAgent.options.rejectUnauthorized = false;
+
 const strapi = require('@strapi/strapi');
 
 const app = strapi.createStrapi({ distDir: './dist' });
